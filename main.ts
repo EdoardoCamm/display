54dc67cdf7a6f6d1b1675251de6d466a243652d7
EdoardoCamm <79929557+EdoardoCamm@users.noreply.github.com>
2021-03-02 12:37:09 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    basic.showNumber(9)
+    basic.showIcon(IconNames.House)
+    basic.showString("Hello there!")
+    basic.pause(1000)
+    basic.showArrow(ArrowNames.SouthEast)
+    basic.pause(500)
+    basic.clearScreen()
+})
